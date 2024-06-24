53ada9a663901f676ac1f0f2048e4f78125fbc4b
John Doe <instagramcloud99@gmail.com>
2024-06-24 13:34:03 +0500

    new update

--- unique_website/settings.py
+++ unique_website/settings.py
@@ -18,7 +18,7 @@ TELEGRAM_CHAT_ID = config('TELEGRAM_CHAT_ID')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['unique-agency.tj', '127.0.0.1']
 
 
 # Application definition
@@ -122,9 +122,10 @@ USE_L10N = True  # Включает локализацию форматов да
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-
+STATIC_URL = '/static/'
+STATIC_ROOT = '/var/www/unique/unique_app/static/'
 MEDIA_URL = '/media/'
+MEDIA_ROOT = '/var/www/unique/unique_website/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
